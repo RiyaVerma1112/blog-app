a12e27a7e8388d6f8d5ae71bb386206f18fecffa
Riya Verma <rv9858196@gmail.com>
2025-01-19 23:21:23 +0530

    code updated 3

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000; 
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const viewsPath = path.join(__dirname, "views");
+console.log("Views Path: ", viewsPath);
+app.set("views", viewsPath);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
